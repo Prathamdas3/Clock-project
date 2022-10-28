41c6cadd52f1181f94367007baeecec649caf9d9
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2022-10-28 07:02:05 -0500

    done the logic writing

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+setInterval(showtime, 1000);
+function showtime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let am_pm = "";
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = "PM";
+  }
+  if (hour == 0) {
+    hour = 12;
+    am_pm = "AM";
+  }
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  let currentTime = hour + ":" + min + ":" + sec + am_pm;
+  document.querySelector(".clock").innerHTML = currentTime;
+}
+showtime();
