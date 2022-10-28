426547145f2aa81ddc31ce8ffc62ff6e4a2e848c
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2022-10-28 07:16:39 -0500

    added the am and pm features

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const showtime = function () {
   let hour = time.getHours();
   let min = time.getMinutes();
   let sec = time.getSeconds();
-  let am_pm = "";
+  let am_pm = "AM";
   if (hour > 12) {
     hour -= 12;
     am_pm = "PM";
@@ -16,7 +16,7 @@ const showtime = function () {
   hour = hour < 10 ? "0" + hour : hour;
   min = min < 10 ? "0" + min : min;
   sec = sec < 10 ? "0" + sec : sec;
-  let currentTime = hour + ":" + min + ":" + sec + am_pm;
+  let currentTime = hour + ":" + min + ":" + sec + " " + am_pm;
   document.querySelector(".clock").innerHTML = currentTime;
 };
 setInterval(showtime, 1000);
